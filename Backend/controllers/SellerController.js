b4d45c15e7650b3af6cffebc5904d8cdf1ab66a4
Arpreet Singh Arora <ar286506@dal.ca>
2022-04-03 01:41:27 -0300

    Adding changes

--- Backend/controllers/SellerController.js
+++ Backend/controllers/SellerController.js
@@ -1,24 +1,56 @@
+
 var AWS = require("aws-sdk");
+var fs = require('fs')
+var 
+
+
+//const multer = require('multer');
+//c/onst path = require('path');
 
 AWS.config.update({
   region: "us-east-1",
-  accessKeyId: "AKIAXKBPMQVLYODWYWXQ",
-  secretAccessKey: "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"
+  accessKeyId: "AKIAXKBPMQVL5WSSAM4H",
+  secretAccessKey: "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"
+});
+
+const s3 = new AWS.S3({
+    accessKeyId: "AKIAXKBPMQVL5WSSAM4H",
+    secretAccessKey: "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"
 });
 
 const storeProducts = async (req,res) =>{   
+    console.log(req);
+    /*const ProductDetails = JSON.parse(JSON.stringify(req.body));
+    console.log(ProductDetails);
+   // const productimage = fs.readFileSync(ProductDetails.productImage);
+    //console.log(productimage);
 
-    var docClient = new AWS.DynamoDB.DocumentClient();
+    const params = {
+        Bucket: 'sellerproductimage',
+        Key: 'img.jpeg', 
+        Body: ProductDetails.productImage
+    };
+
+    s3.upload(params, function(err, data) {
+        if (err) {
+            throw err;
+        }
+    const datajson = {
+        "s3uri":data.Location
+    }
+
+        res.send(datajson);
+
+    });
+
+
+    /*var docClient = new AWS.DynamoDB.DocumentClient();
     var table = "Products";   
 
     var params = {
         TableName:table,
         Item:{
-            "ProductId" : "5",
-            "ProductName" : "Mutton",
-            "Brand":"Apple",
-            "Quantity": 10,
-            "ProductImage":"abc"
+            ...ProductDetails
             }
         }
    
@@ -27,8 +59,26 @@ const storeProducts = async (req,res) =>{
         docClient.put(params)
                 .promise()
                     .then(data => res.send({Status:"Item added"}))
-                        .catch(console.error);
+                        .catch(console.error);*/
     
 };
+
+/*const uploadingProducts = (req,res)=>{
+
+    const storage = multer.diskStorage({
+        destination: '../Images/Uploads',
+        filename: function(req, file, cb){
+            cb(null, productname + path.extname(file.originalname));
+        }
+    })
+
+    const upload = multer({
+        storage : storage,
+    }).single('productImage')
+
+    upload(req,res, (err) =>{
+        console.log(req.file);
+    })
+}*/
 
 module.exports = {storeProducts};
--- Backend/routes/SellerRoute.js
+++ Backend/routes/SellerRoute.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const sellerController = require('../controllers/SellerController');
 
 
-router.post("/api/vi/Seller",sellerController.storeProducts);
+
+router.post("/api/vi/Seller/createNewProduct",sellerController.storeProducts);
+//router.post("/api/vi/Seller/productImages", sellerController.storeImages);
 
 module.exports = router;
